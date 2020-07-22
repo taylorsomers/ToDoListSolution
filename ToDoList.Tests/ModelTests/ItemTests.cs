8600990b1493d3ecc358dbb3a2ca5b2fb636c823
Noel Kirkland <noelkirkland@gmail.com>
2020-07-22 10:30:45 -0700

    implement auto ID assigning to our object and have written two tests for this new property, one passing, one failing
    Co-authored-by: Taylor Somers <tsomers@yandex.com>

--- ToDoList.Tests/ModelTests/ItemTests.cs
+++ ToDoList.Tests/ModelTests/ItemTests.cs
@@ -80,5 +80,29 @@ namespace ToDoList.Tests
       //Assert
       CollectionAssert.AreEqual(newList, result);
     }
+
+    [TestMethod]
+    public void GetId_ItemsInstantiateWithAnIdAndGetterReturns_Int()
+    {
+      string description = "Walk the dog.";
+      Item newItem = new Item(description);
+
+      int result = newItem.Id;
+
+      Assert.AreEqual(1, result);
+    }
+
+    [TestMethod]
+    public void Find_ReturnsCorrectItem_Item()
+    {
+      string description01 = "Walk the dog";
+      string description02 = "Wash the dishes";
+      Item newItem1 = new Item(description01);
+      Item newItem2 = new Item(description02);
+
+      Item result = Item.Find(2);
+
+      Assert.AreEqual(newItem2, result);
+    }
   }
 }
