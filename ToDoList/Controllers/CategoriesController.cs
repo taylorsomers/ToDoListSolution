23c18cd210d6d0ee47bb5ce98332716fe7288f8a
Taylor Somers <tsomers@yandex.com>
2020-07-23 13:02:13 -0400

    Add code for categories controllers and index/new categories views
    Co-authored-by: Evgeniya Chernaya <evgenya.chernaya@gmail.com>

--- ToDoList/Controllers/CategoriesController.cs
+++ ToDoList/Controllers/CategoriesController.cs
@@ -7,6 +7,16 @@ namespace ToDoList.Controllers
 {
   public class CategoriesController : Controller
   {
-    
+    [HttpGet("/categories")]
+    public ActionResult Index()
+    {
+      List<Category> allCategories = Category.GetAll();
+      return View(allCategories);
+    }
+    [HttpGet("/categories/new")]
+    public ActionResult New()
+    {
+      return View();
+    }
   }
 }
