f1d54890a4cad2841af61c5d43808344d2e05604
Taylor Somers <tsomers@yandex.com>
2020-07-23 10:28:02 -0400

    Add clear category functionality

--- ToDoList.Tests/ModelTests/CategoryTests.cs
+++ ToDoList.Tests/ModelTests/CategoryTests.cs
@@ -6,13 +6,37 @@ using System;
 namespace ToDoList.Tests
 {
   [TestClass]
-  public class CategoryTest
+  public class CategoryTest : IDisposable
   {
+    public void Dispose()
+    {
+      Category.ClearAll();
+    }
     [TestMethod]
     public void CategoryConstructor_CreatesInstancesOfCategory_Category()
     {
       Category newCategory = new Category("test category");
       Assert.AreEqual(typeof(Category), newCategory.GetType());
+    }
+    [TestMethod]
+    public void GetName_ReturnsName_String()
+    {
+      string name = "Test Category";
+      Category newCategory = new Category(name);
+
+      string result = newCategory.Name;
+
+      Assert.AreEqual(name, result);
+    }
+    [TestMethod]
+    public void GetId_ReturnsCategoryId_Int()
+    {
+      string name = "Test Category";
+      Category newCategory = new Category(name);
+
+      int result = newCategory.Id;
+
+      Assert.AreEqual(1, result);
     }
   }
 }
