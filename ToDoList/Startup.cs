bfdd796ca4cda7621701a3e07a31a51734d8932c
Taylor Somers <tsomers@yandex.com>
2020-07-28 10:35:44 -0400

    Add using Microsoft.EntityFrameworkCore

--- ToDoList/Startup.cs
+++ ToDoList/Startup.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
