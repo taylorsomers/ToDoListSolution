c1102d43abf1bf1497ec4cc6bb09eba834b8e628
Taylor Somers <tsomers@yandex.com>
2020-07-29 10:25:33 -0400

    Add Edit methods and Microsoft Entity Framework using directive

--- ToDoList/Controllers/ItemsController.cs
+++ ToDoList/Controllers/ItemsController.cs
@@ -1,7 +1,8 @@
 using Microsoft.AspNetCore.Mvc;
-using ToDoList.Models;
+using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Linq;
+using ToDoList.Models;
 
 namespace ToDoList.Controllers
 {
@@ -37,6 +38,20 @@ namespace ToDoList.Controllers
     {
       Item thisItem = _db.Items.FirstOrDefault(items => items.ItemId == id);
       return View(thisItem);
+    }
+
+    public ActionResult Edit(int id)
+    {
+      var thisItem = _db.Items.FirstOrDefault(items => items.ItemId == id);
+      return View(thisItem);
+    }
+
+    [HttpPost]
+    public ActionResult Edit(Item item)
+    {
+      _db.Entry(item).State = EntityState.Modified;
+      _db.SaveChanges();
+      return RedirectToAction("Index");
     }
   }
 }
